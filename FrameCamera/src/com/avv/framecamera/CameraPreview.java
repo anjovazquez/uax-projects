96967f2d7a9a84ab813dd691201efe3d367fc16d
angelvazquez <anjo.vazquez@gmail.com>
2015-01-13 20:45:01 +0100

    Para entrega

--- FrameCamera/src/com/avv/framecamera/CameraPreview.java
+++ FrameCamera/src/com/avv/framecamera/CameraPreview.java
@@ -16,6 +16,13 @@ import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
+/**
+ * Vista que hemos creado en la que encapsulamos la superficie de
+ * previsualización de la cámara
+ * 
+ * @author angelvazquez
+ * 
+ */
 public class CameraPreview extends FrameLayout implements
 		SurfaceHolder.Callback {
 
@@ -23,105 +30,138 @@ public class CameraPreview extends FrameLayout implements
 	private Camera camera;
 
 	private final int REAR_CAMERA = 0;
-	private final int FRONT_CAMERA = 1;
-	private int activeCameraId = REAR_CAMERA;
+	private final int activeCameraId = this.REAR_CAMERA;
 
+	/*
+	 * Creamos la cámara y encapsulamos la superficie de previsualización y
+	 * suscribimos nuestra vista a los cambios en la misma
+	 */
 	public CameraPreview(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		createCamera();
-		if (camera == null) {
+		this.createCamera();
+		if (this.camera == null) {
 			return;
 		}
+		this.surfaceView = new SurfaceView(context);
+		this.addView(this.surfaceView);
 
-		surfaceView = new SurfaceView(context);
-		addView(surfaceView);
-
-		SurfaceHolder holder = surfaceView.getHolder();
+		SurfaceHolder holder = this.surfaceView.getHolder();
 		holder.addCallback(this);
 		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 		holder.setKeepScreenOn(true);
 	}
 
+	/*
+	 * Si no existe la cámara comprobamos que la cámara está presente en el
+	 * dispositivo y si está disponible si ya existe paramos la previsualización
+	 * y liberamos
+	 */
 	public void createCamera() {
-		if (camera != null) {
-			camera.stopPreview();
-			camera.release();
+		if (this.camera != null) {
+			this.camera.stopPreview();
+			this.camera.release();
 		}
 
-		if (getContext().getPackageManager().hasSystemFeature(
-				PackageManager.FEATURE_CAMERA)) {
+		if (this.getContext().getPackageManager()
+				.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
 			try {
-				this.camera = Camera.open(activeCameraId);
+				this.camera = Camera.open(this.activeCameraId);
 			} catch (Exception e) {
-				Toast.makeText(getContext(), "Error al abrir la cámara",
+				Toast.makeText(this.getContext(), "Error al abrir la cámara",
 						Toast.LENGTH_LONG).show();
 				return;
 			}
 		} else {
-			Toast.makeText(getContext(), "No hay cámara", Toast.LENGTH_LONG)
-					.show();
+			Toast.makeText(this.getContext(), "No hay cámara",
+					Toast.LENGTH_LONG).show();
 			return;
 		}
 	}
 
 	public Camera getCamera() {
-		return camera;
+		return this.camera;
 	}
 
+	/*
+	 * Comprobamos si la cámara está disponible en cuanto se crea la superficie
+	 * de previsualización y lo relacionamos con la cámara (non-Javadoc)
+	 * 
+	 * @see
+	 * android.view.SurfaceHolder.Callback#surfaceCreated(android.view.SurfaceHolder
+	 * )
+	 */
+	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
-		try {
-			camera.setPreviewDisplay(holder);
-			camera.startPreview();
-		} catch (Exception e) {
-			Log.d("Error al poner la vista previa de la cámara: ",
-					e.getMessage());
+		if (this.getCamera() != null) {
+			try {
+				this.camera.setPreviewDisplay(holder);
+				this.camera.startPreview();
+			} catch (Exception e) {
+				Log.d("Error al poner la vista previa de la cámara: ",
+						e.getMessage());
+			}
 		}
 	}
 
+	/*
+	 * Si cambia la superficie para que mantenga la relación de aspecto
+	 * 
+	 * @see
+	 * android.view.SurfaceHolder.Callback#surfaceChanged(android.view.SurfaceHolder
+	 * , int, int, int)
+	 */
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
 			int height) {
-		// TODO Auto-generated method stub
-		if(holder.getSurface() == null){
+		if (holder.getSurface() == null) {
 			return;
 		}
-		
-		camera.stopPreview();
-		
-		Display display = ((WindowManager) this.getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+
+		this.camera.stopPreview();
+
+		Display display = ((WindowManager) this.getContext().getSystemService(
+				Context.WINDOW_SERVICE)).getDefaultDisplay();
 		CameraInfo info = new CameraInfo();
 		Camera.getCameraInfo(0, info);
 		int cameraOrientation = info.orientation;
-		
-		Size cameraSize = camera.getParameters().getPreviewSize();
-		
-//		if(display.getRotation() == Surface.ROTATION_0 || display.getRotation() == Surface.ROTATION_180 && (cameraOrientation == 90 || cameraOrientation == 270)){
-//			cameraSize.width = camera.getParameters().getPreviewSize().height;
-//			cameraSize.height = camera.getParameters().getPreviewSize().width;
-//			
-//			camera.setDisplayOrientation(90);
-//			
-//			float ratio = (float) cameraSize.width / (float) cameraSize.height;
-//			
-//			FrameLayout.LayoutParams params = new FrameLayout.LayoutParams((int) (height*ratio), height, Gravity.CENTER);
-//			surfaceView.setLayoutParams(params);
-			
-			try{
-				camera.setPreviewDisplay(holder);
-				camera.startPreview();
-			} catch (Exception e){
-				Log.d("CameraPreview", "Error al mostrar vista previa: "+e.getMessage());
-			}
-		
-//		}
+		Size cameraSize = this.camera.getParameters().getPreviewSize();
+		if (((display.getRotation() == Surface.ROTATION_0) || ((display
+				.getRotation() == Surface.ROTATION_180)))
+				&& ((cameraOrientation == 90) || (cameraOrientation == 270))) {
+			cameraSize.width = this.camera.getParameters().getPreviewSize().height;
+			cameraSize.height = this.camera.getParameters().getPreviewSize().width;
+
+			this.camera.setDisplayOrientation(90);
+		}
+
+		float ratio = (float) cameraSize.width / (float) cameraSize.height;
+
+		FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+				(int) (height * ratio), height, Gravity.CENTER);
+		this.surfaceView.setLayoutParams(params);
+
+		try {
+			this.camera.setPreviewDisplay(holder);
+			this.camera.startPreview();
+		} catch (Exception e) {
+			Log.d("CameraPreview",
+					"Error al mostrar vista previa: " + e.getMessage());
+		}
 	}
 
+	/*
+	 * Paramos la previsualización y liberamos la cámara si la superficie se
+	 * destruye si se sale de la actividad por ejemplo
+	 * 
+	 * @see android.view.SurfaceHolder.Callback#surfaceDestroyed(android.view.
+	 * SurfaceHolder)
+	 */
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		// TODO Auto-generated method stub
-		camera.stopPreview();
-		camera.release();
-		camera = null;
+		this.camera.stopPreview();
+		this.camera.release();
+		this.camera = null;
 
 		holder.removeCallback(this);
 	}
--- FrameCamera/src/com/avv/framecamera/MainActivity.java
+++ FrameCamera/src/com/avv/framecamera/MainActivity.java
@@ -8,8 +8,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.graphics.Bitmap;
-import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Matrix;
@@ -17,45 +17,86 @@ import android.hardware.Camera;
 import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+/**
+ * La actividad principal que controla la cámara
+ * 
+ * @author angelvazquez
+ * 
+ */
 public class MainActivity extends Activity implements PictureCallback {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		this.setContentView(R.layout.activity_main);
 	}
 
+	/*
+	 * Método que se lanza cuando se presiona el botón de sacar foto declarado
+	 * en el XML en el evento onClick
+	 */
 	public void takePhoto(View button) {
-		((CameraPreview) findViewById(R.id.camera_preview)).getCamera()
+		((CameraPreview) this.findViewById(R.id.camera_preview)).getCamera()
 				.takePicture(null, null, this);
 	}
 
+	/*
+	 * Método callback que implementamos con la interfaz PictureCallback en el
+	 * cual escribimos que queremos hacer con la foto tomada
+	 */
+
 	@Override
 	public void onPictureTaken(byte[] data, Camera camera) {
-		// TODO Auto-generated method stub
+
+		String photoPath = this.generateName();
+		Bitmap bitmapDecorate = this.composeBitmap(data);
+		this.saveImage(photoPath, bitmapDecorate);
+		this.addToGallery(photoPath);
+
+		Bitmap bitmap = BitmapFactory.decodeFile(photoPath);
+		((ImageView) this.findViewById(R.id.imageView)).setImageBitmap(bitmap);
+
+		camera.startPreview();
+	}
+
+	/*
+	 * Decodificamos los datos que se nos devuelven tras la foto tomada por la
+	 * cámara
+	 */
+	private Bitmap composeBitmap(byte[] data) {
+		Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+		bitmap = this.overlay(bitmap);
+		return bitmap;
+	}
+
+	/*
+	 * Generamos un nombre a partir de la fecha actual con precisión hasta los
+	 * segundos
+	 */
+	private String generateName() {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyymmddhhmmss");
 		String date = dateFormat.format(new Date());
 		String photoFile = "picture_" + date + ".jpg";
 		String photoPath = Environment.getExternalStorageDirectory()
 				.getAbsolutePath() + File.separator + photoFile;
+		return photoPath;
+	}
 
-		Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
-		
-		Config config = bitmap.getConfig();
-		Bitmap bitmapDecorate = Bitmap.createBitmap(
-				new Double(bitmap.getWidth() * 1.2).intValue(), new Double(
-						bitmap.getHeight() * 1.2).intValue(), config);
-		bitmap = overlay(bitmapDecorate, bitmap);
-		
+	/*
+	 * Guarda la imagen en el dispositivo
+	 */
+	private void saveImage(String photoPath, Bitmap bitmap) {
 		ByteArrayOutputStream stream = new ByteArrayOutputStream();
-		bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+		bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
 		byte[] byteArray = stream.toByteArray();
-		
+
 		try {
 			FileOutputStream fos = new FileOutputStream(photoPath);
 			fos.write(byteArray);
@@ -64,29 +105,46 @@ public class MainActivity extends Activity implements PictureCallback {
 			Toast.makeText(this, "No se pudo guardar el archivo",
 					Toast.LENGTH_LONG).show();
 		}
-
 		Toast.makeText(this, "La foto se guardó en: " + photoPath,
 				Toast.LENGTH_LONG).show();
-
-//		Bitmap bitmap = BitmapFactory.decodeFile(photoPath);
-		((ImageView) findViewById(R.id.imageView)).setImageBitmap(bitmap);
-
-		
-
-		camera.startPreview();
 	}
 
-	private Bitmap overlay(Bitmap bmp1, Bitmap bmp2) {
-		Bitmap bmOverlay = Bitmap.createBitmap(bmp1.getWidth(),
-				bmp1.getHeight(), bmp1.getConfig());
+	/*
+	 * Es necesario notificarlo para que se pueda ver desde la galería
+	 */
+	private void addToGallery(String photoPath) {
+		ContentValues values = new ContentValues();
+
+		values.put(Images.Media.DATE_TAKEN, System.currentTimeMillis());
+		values.put(Images.Media.MIME_TYPE, "image/jpeg");
+		values.put(MediaStore.MediaColumns.DATA, photoPath);
+
+		MainActivity.this.getContentResolver().insert(
+				Images.Media.EXTERNAL_CONTENT_URI, values);
+	}
+
+	/*
+	 * Creamos un bitmap un poco mayor que el decodificado a partir de los datos
+	 * que nos envia la cámara y lo pintamos en un canvas, pintamos el fondo del
+	 * color del marco y pintamos nuestro bitmap dentro a partir de las
+	 * coordenadas (cx,cy) con lo cual nos quedará nuestro bitmap con un marco
+	 * que lo rodea del color pintado
+	 */
+	private Bitmap overlay(Bitmap bitmap) {
+
+		Bitmap bmOverlay = Bitmap.createBitmap(new Double(
+				bitmap.getWidth() * 1.2).intValue(),
+				new Double(bitmap.getHeight() * 1.2).intValue(), bitmap
+						.getConfig());
 		Canvas canvas = new Canvas(bmOverlay);
-		canvas.drawColor(getResources().getColor(android.R.color.holo_blue_bright));		
-		
-		int cx = (bmp1.getWidth() - bmp2.getWidth()) >> 1; 
-	    int cy = (bmp1.getHeight() - bmp2.getHeight()) >> 1;
-	    
-		canvas.drawBitmap(bmp1, new Matrix(), null);
-		canvas.drawBitmap(bmp2, cx, cy, null);
+		canvas.drawColor(this.getResources().getColor(
+				android.R.color.holo_blue_bright));
+
+		int cx = (bmOverlay.getWidth() - bitmap.getWidth()) >> 1;
+		int cy = (bmOverlay.getHeight() - bitmap.getHeight()) >> 1;
+
+		canvas.drawBitmap(bmOverlay, new Matrix(), null);
+		canvas.drawBitmap(bitmap, cx, cy, null);
 		return bmOverlay;
 	}
 
